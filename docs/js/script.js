6e4d27756b9791350113cecef7b89387878bd560
Emma Pilkington <epilk@hotmail.com>
2021-05-01 22:59:52 +0200

    Improve speech recognition

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,69 +1,88 @@
-var recognition;
+window.onload = () => {
+	/* SpeechRecognition variables */
+	var final_transcript = '';
+	var listening = false;
+	var recognition;
 
-var chatLog = document.getElementById('chat-log');
-var final_transcript = '';
+	/* Element References */
+	var chatLog = document.getElementById('chat-log');
+	var chatInput = document.getElementById('chat-input');
+	var micButton = document.getElementById('mic');
 
-function scrollChat() {
-	setTimeout(() => {
+	function scrollChat() {
 		chatLog.scrollTop = chatLog.scrollHeight;
-	}, 500);
-}
-
-scrollChat();
-
-var listening = false;
-var pttButton = document.getElementById('mic');
-pttButton.onmousedown = () => {
-	if(!listening) {
-		final_transcript = '';
-		listening = true;
-		recognition.start();
-	} else {
-		listening = false;
-		recognition.stop();
 	}
-}
-pttButton.onmouseup = () => {
-	listening = false;
-	recognition.stop();
-}
-
-var chatInput = document.getElementById('chat-input');
-
-var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
-if (SpeechRecognition) {
-	recognition = new SpeechRecognition();
-	recognition.continuous = true;
-	recognition.interimResults = true;
+	scrollChat(); /* Scroll chat on load */
 	
-	recognition.onstop = (event) => {
-		// <p class="message"><span class="name">Kitaki:</span> i cant see it</p>
+	function sendMessage() { /* Send message from chatInput to chatLog*/
+		final_transcript = '';	
+		/* Create message element */		
+		var message = document.createElement('p');
+		message.classList.add('message');
+		/* Add name to start of message */
+		var name = document.createElement('span');
+		name.classList.add('name3');
+		name.innerText = 'You: ';
+		message.append(name);
 		
+		/* Add chatInput to message */
+		message.append(chatInput.value);
+		chatInput.value = '';
+		
+		/* Add message to chatLog */
+		document.getElementById('chat-log').append(message);
+		scrollChat();
 	}
 
-	recognition.onresult = function(event) {
-	  var interim_transcript = '';
-	  for (var i = event.resultIndex; i < event.results.length; ++i) {
-		if (event.results[i].isFinal) {
-		  final_transcript += event.results[i][0].transcript;
-		} else {
-		  interim_transcript += event.results[i][0].transcript;
+
+	if (!('webkitSpeechRecognition' in window) && !('SpeechRecognition' in window)) {
+		/* If SpeechRecognition is not supported */
+		micButton.disabled = true;
+	} else {
+		/* On click start */
+		micButton.onmousedown = () => {
+				listening = true;
+				micButton.classList.add('blink-red');
+				final_transcript = '';
+				recognition.lang = 'en-US';
+				recognition.start();
 		}
-	  }
-	  chatInput.value = interim_transcript;
-	  if(final_transcript) {
-			chatInput.value = '';
-			var message = document.createElement('p');
-			message.classList.add('message');
-			var name = document.createElement('span');
-			name.classList.add('name3');
-			name.innerText = 'You:';
-			message.append(name);
-			message.append(' ' + final_transcript);
-			document.getElementById('chat-log').append(message);
-			scrollChat();
-	  };
-	};
-} else {
-	pttButton.disabled = true;
+		/* On click released */
+		micButton.onmouseup = () => {
+			setTimeout(() => {
+				listening = false;
+				micButton.classList.remove('blink-red');
+				if(chatInput.value) { /* Send all non-empty messages */
+					sendMessage();
+				}
+			}, 1000); /* Allow an extra second to finish speechRecognition */
+		}
+
+		var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+			if (SpeechRecognition) {
+				recognition = new SpeechRecognition();
+				recognition.continuous = true;
+				recognition.interimResults = true;
+
+				recognition.onerror = () => {
+					listening = false;
+					micButton.classList.remove('blink-red');
+				}
+
+				recognition.onresult = function (event) {
+					var interim_transcript = '';
+					for (var i = event.resultIndex; i < event.results.length; ++i) {
+						if (event.results[i].isFinal) {
+							final_transcript += event.results[i][0].transcript;
+						} else {
+							interim_transcript += event.results[i][0].transcript;
+						}
+					}
+					if(!listening) return; /* Don't add to chatInput if we're not listening */
+					chatInput.value = final_transcript + interim_transcript;
+					chatInput.scrollLeft = chatInput.scrollWidth;
+					if(final_transcript) sendMessage(); /* Automatically send message after a pause */
+				};
+			}
+	}
 }
--- docs/js/welcome.js
+++ docs/js/welcome.js
@@ -1,48 +1,80 @@
-var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
-if (SpeechRecognition) {
-	var recognition = new SpeechRecognition();
-	recognition.continuos = true;
-	recognition.interimResults = true;
-	recognition.start();
-	recognition.stop();
-} else {
-	var button = document.getElementById('continue');
-	button.innerText = "Your browser is not supported";
-	button.disabled = true;
-}
-
-function nextPage() {
-	createCookie('doRedirect','true','999');
-	window.location = 'app';
-}
-
-function redirect(){
-	var thecookie = readCookie('doRedirect');
-	if(thecookie){
-		nextPage();
-	}
-}
-
-function createCookie(name,value,days){
-	if (days){
-		var date = new Date();
-		date.setTime(date.getTime()+(days*24*60*60*1000));
-		var expires = "; expires="+date.toGMTString();
-	} else var expires = "";
-	document.cookie = name+"="+value+expires+"; path=/";
-}
-
-function readCookie(name){
-	var nameEQ = name + "=";
-	var ca = document.cookie.split(';');
-	for(var i=0;i < ca.length;i++){
-		var c = ca[i];
-		while (c.charAt(0)==' ') c = c.substring(1,c.length);
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-	}
-	return null;
-}
-
-window.onload = function(){
+window.onload = function () {
 	redirect();
+	
+	var button = document.getElementById('continue');
+
+	if (!('webkitSpeechRecognition' in window)) { // && !('SpeechRecognition' in window)) {
+		upgrade();
+	} else {
+		var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+			if (SpeechRecognition) {
+				var recognition = new SpeechRecognition();
+				recognition.continuos = true;
+				recognition.interimResults = true;
+
+				recognition.onerror = function (event) {
+					console.log(event);
+					showInfo('info_error');
+					button.disabled = true;
+				};
+				recognition.start();
+			}
+	}
+
+	function upgrade() {
+		button.disabled = true;
+		showInfo('info_upgrade');
+	}
+
+
+	function showInfo(s) {
+	var info = document.getElementById('info');
+		if (s) {
+			for (var child = info.firstChild; child; child = child.nextSibling) {
+				console.log(child);
+				if (child.style) {
+					child.style.display = child.id == s ? 'inline' : 'none';
+				}
+			}
+			info.style.display = 'block';
+		} else {
+			info.style.display = 'none';
+		}
+	}
+
+	function nextPage() {
+		createCookie('doRedirect', 'true', '999');
+		window.location = 'app';
+	}
+
+	function redirect() {
+		var thecookie = readCookie('doRedirect');
+		if (thecookie) {
+			nextPage();
+		}
+	}
+
+	function createCookie(name, value, days) {
+		if (days) {
+			var date = new Date();
+			date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+			var expires = "; expires=" + date.toGMTString();
+		} else
+			var expires = "";
+		document.cookie = name + "=" + value + expires + "; path=/";
+	}
+
+	function readCookie(name) {
+		var nameEQ = name + "=";
+		var ca = document.cookie.split(';');
+		for (var i = 0; i < ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0) == ' ')
+				c = c.substring(1, c.length);
+			if (c.indexOf(nameEQ) == 0)
+				return c.substring(nameEQ.length, c.length);
+		}
+		return null;
+	}
+
 }
